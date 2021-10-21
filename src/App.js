f9949f3358dffe9c87f68b7b52c52ee642f4cb9d
Chris Thomason <christhomason@Chriss-MacBook-Pro-2.local>
2021-10-21 12:56:43 +0100

    Restarted the project to create a new Meetups style website. Added in React Router Dom and Linked together a Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo title="Todo #1"/>
-      <Todo title="Todo #2"/>
-      <Todo title="Todo #3" />
-      <Todo title="Todo #4"/>
-      
-      
+      <MainNavigation />
+      <Switch> 
+      <Route path ='/' exact>
+        <AllMeetupsPage />
+      </Route>
+
+      <Route path = '/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+
+      <Route path = '/favorites'>
+        <FavoritesPage />
+      </Route>
+      </Switch> 
+
     </div> 
   );
 }
